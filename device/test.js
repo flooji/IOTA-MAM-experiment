21282d0a6b7577e3aa788c5d4c64208269af79df
flooji <florella240@gmail.com>
2020-01-01 20:34:56 +0100

    write file

--- device/test.js
+++ device/test.js
@@ -3,6 +3,11 @@ var file = '/dev/ttyS0'
 const SerialPort = require('serialport')
 const parsers = SerialPort.parsers
 
+const fs = require('fs')
+
+//Interval of getting GPS data
+const interval = 15 //every x sec
+
 /*
 SerialPort.list(function (err, ports) {
   console.log(ports);
@@ -23,10 +28,33 @@ port.pipe(parser)
 var GPS = require('gps')
 var gps = new GPS
 
-gps.on('data', function(data) {
-  console.log(data)
-})
+//Get single parameters from GPS state object
+const getGPS = () => {
+
+  const packet = {
+      time:   gps.state.time,
+      lat:    gps.state.lat,
+      lon:    gps.state.lon,
+      alt:    gps.state.alt,
+      speed:  gps.state.speed,  
+      processedLines: gps.state.processed,
+  }
+
+  // Save new packet
+  fs.writeFileSync('gps.json',JSON.stringify(packet))
+  console.log(gps.state)
+  console.log(packet)
+}
+
+// gps.on('data', function(data) {
+//   console.log(data)
+// })
 
 parser.on('data', function(data) {
   gps.update(data)
 })
+
+getGPS()
+
+//Set interval to get GPS data
+setInterval(getGPS,interval*1000)
